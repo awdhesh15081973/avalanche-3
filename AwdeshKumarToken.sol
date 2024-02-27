6417be6a2bb0ae4b8dfb9a90e6f9693333e22e55
awdhesh15081973 <155299006+awdhesh15081973@users.noreply.github.com>
2024-02-27 22:55:18 +0530

    Update AwdeshKumarToken.sol

--- AwdeshKumarToken.sol
+++ AwdeshKumarToken.sol
@@ -3,17 +3,17 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts@5.0.1/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts@5.0.1/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@5.0.1/access/Ownable.sol";
 
-contract AwdeshKumar is ERC20("AwdeshKumar", "AWD"), ERC20Burnable {
-    constructor()
+contract SpiderMan is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner)
+        ERC20("SpiderMan", "Peter")
+        Ownable(initialOwner)
     {
-        _mint(msg.sender,2 * 10 ** decimals());
+        _mint(msg.sender,1 * 10 ** decimals());
     }
 
-    function mint(address addr, uint256 amt) public  {
-        _mint(addr, amt);
-    }
-    function burn(address addr, uint256 amt) public  {
-        _burn(addr, amt);
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
